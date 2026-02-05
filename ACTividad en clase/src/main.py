18b6d2521cf7a9987d3650cb4ac50757f16c030b
GCARLOS <quinterogiancarlos28@gmail.com>
2026-02-05 14:00:03 -0500

    funciones_operarias

---
+++ ACTividad en clase/src/main.py
@@ -0,0 +1,13 @@
+
+def saludo():
+    print("programa para encontrar el area ")
+
+
+var = input("ingrese la base  ")
+var2= input("ingrese la altura ")
+resultado = float(var) * float(var2) / 2
+
+saludo()
+print("el area del triangulo es: " , resultado)
+
+print(type(resultado))
